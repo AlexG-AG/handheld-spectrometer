8289291f336c36c3a5c2992094fa8b35b13e86ee
apgar <Alex.Gareau@dal.ca>
2026-04-23 14:10:22 -0300

    Added Readme, reorganized a bit

--- measure-spectrum.py
+++ measure-spectrum.py
@@ -4,21 +4,24 @@ import seabreeze
 seabreeze.use('pyseabreeze')
 from seabreeze.spectrometers import Spectrometer
 
-integtime = 20000
-scans = 5
-intensities = []
+def get_spectrum(integtime, scans):
+    intensities = []
 
-spec = Spectrometer.from_first_available()
-spec.integration_time_micros(integtime)
-wavelengths = spec.wavelengths()
+    spec = Spectrometer.from_first_available()
+    spec.integration_time_micros(integtime)
+    wavelengths = spec.wavelengths()
 
-for i in range(0, scans):
-    intensities.append(spec.intensities())
+    for i in range(0, scans):
+        intensities.append(spec.intensities())
 
-avgintensities = np.average(intensities)
+    print(intensities)
+    avgintensities = np.average(intensities)
 
-fig, ax = plt.subplots()
+    fig, ax = plt.subplots()
 
-ax.plot(wavelengths, avgintensities, color="blue", linestyle="-", label="Normal")
-plt.show()
-plt.close()
+    ax.plot(wavelengths, avgintensities, color="blue", linestyle="-", label="Normal")
+    plt.show()
+    plt.close()
+
+def main():
+    get_spectrum(20000, 5)
