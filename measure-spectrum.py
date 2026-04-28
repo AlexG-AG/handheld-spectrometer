4b9af970b2efa589282943877d85b590795aee1e
apgar <Alex.Gareau@dal.ca>
2026-04-28 14:01:17 -0300

    Added first implementation of calibrate_spectrum method, gitignore

--- measure-spectrum.py
+++ measure-spectrum.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datetime as dt
 import os
+import math
 import seabreeze
 seabreeze.use('pyseabreeze')
 from seabreeze.spectrometers import Spectrometer
@@ -26,10 +27,31 @@ def get_spectrum(integtime, scans):
 
     return spec, spectrum
 
-def calibrate_spectrum(spectrum):
+def calibrate_spectrum(spectrometer, spectrum):
     # Need to load a calibration (based on the spectrometer being used?) and scale the spectrum accordingly
     # Calibration file and spectrum wavelengths mat not necessarily be the same: USB4000 calibration using OceanView only covers up to 906nm, but the script measures up to 940nm. This needs to be handled, hopefully regardless of spectrometer.
-    cald_spectrum = spectrum
+    spectrometer_name = str(spectrometer).split(":")[0].split(" ")[1]
+
+    calibration_path = os.path.join(".", "calibrations", spectrometer_name)
+    for file in os.listdir(calibration_path):
+        if file.endswith(".cal"):
+            filepath = os.path.join(calibration_path, file)
+            break
+
+    cald_spectrum = [[], []]
+
+    with open(filepath, "r") as f:
+        #parse calibration file
+        cal_data = f.readlines()[9:]
+        for i in range(0, len(cal_data)):
+            wavelength = cal_data[i].split('\t')[0]
+            intensity_scaling = cal_data[i].split('\t')[1]
+            if math.isclose(spectrum[0][i], float(wavelength)):
+                cald_spectrum[0].append(spectrum[0][i])
+                cald_spectrum[1].append(spectrum[1][i]*float(intensity_scaling))
+        
+    f.close()
+
     return cald_spectrum
 
 def integrate_spectrum(spectrum):
@@ -38,9 +60,9 @@ def integrate_spectrum(spectrum):
     intensities = spectrum[1]
 
     # Calculate the absolute difference between each array entry, then return the index of the smallest difference (The closest match)
-    point1 = np.abs(wavelengths - 350).argmin()
-    point2 = np.abs(wavelengths - 420).argmin()
-    point3 = np.abs(wavelengths - 550).argmin()
+    point1 = np.abs(np.subtract(wavelengths, 350)).argmin()
+    point2 = np.abs(np.subtract(wavelengths, 420)).argmin()
+    point3 = np.abs(np.subtract(wavelengths, 550)).argmin()
 
     violet_wavelengths = wavelengths[point1:(point2 + 1)]
     blue_wavelengths = wavelengths[point2:(point3 + 1)]
@@ -57,18 +79,19 @@ def integrate_spectrum(spectrum):
     return violet_power, blue_power, total_power
 
 def save_results(spectrometer, spectrum, violet_power, blue_power, total_power):
-
-    foldername = dt.date.today().strftime("./output/%d-%m-%Y")
+    # Need to fix os.path.join usage
+    today = dt.date.today().strftime("%d-%m-%Y")
+    foldername = os.path.join(".", "output", today)
     if os.path.exists(foldername) == False: 
         os.mkdir(foldername)
 
-    filename = dt.datetime.now().strftime("%d-%m-%Y--%H-%M-%S")
-    filepath = os.path.join(foldername + "/" + filename + ".txt")
+    filename = dt.datetime.now().strftime("%d-%m-%Y--%H-%M-%S.txt")
+    filepath = os.path.join(foldername, filename)
     with open(filepath, "w") as f:
         f.write(f"{spectrometer}\n----------\n")
-        f.write(f"350-420nm Power: {violet_power}\n")
-        f.write(f"420-550nm Power: {blue_power}\n")
-        f.write(f"350-550nm Power: {total_power}\n")
+        f.write(f"350-420nm Power (mW): {violet_power}\n")
+        f.write(f"420-550nm Power (mW): {blue_power}\n")
+        f.write(f"350-550nm Power (mW): {total_power}\n")
         f.write(f"Spectrum:\n")
         for i in range(0, len(spectrum[0])):
             f.write(f"{spectrum[0][i]}\t{spectrum[1][i]}\n")
@@ -79,7 +102,7 @@ def save_results(spectrometer, spectrum, violet_power, blue_power, total_power):
 
 def main():
     spectrometer, spectrum = get_spectrum(20000, 5)
-    cald_spectrum = calibrate_spectrum(spectrum)
+    cald_spectrum = calibrate_spectrum(spectrometer, spectrum)
     violet_power, blue_power, total_power = integrate_spectrum(cald_spectrum)
     save_results(spectrometer, cald_spectrum, violet_power, blue_power, total_power)
     
